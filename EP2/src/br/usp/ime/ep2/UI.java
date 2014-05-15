2f46f82781b0d569716f64e07b6724bdb2aac619
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2014-05-15 12:17:41 -0300

    Remove unused impot

--- EP2/src/br/usp/ime/ep2/UI.java
+++ EP2/src/br/usp/ime/ep2/UI.java
@@ -4,7 +4,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Activity;
-import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.TextView;
