80232ec3ee47dbb50222ab0346e9d9a18c205614
Hugo Braga <hugobraga@gmail.com>
2014-05-15 22:17:16 -0300

    Restored explosive brick probability's value.

--- EP2/src/br/usp/ime/ep2/forms/Brick.java
+++ EP2/src/br/usp/ime/ep2/forms/Brick.java
@@ -2,7 +2,7 @@ package br.usp.ime.ep2.forms;
 
 public class Brick extends Quad {
 	
-	public static final float GRAY_BRICK_PROBABILITY = 0.6f;
+	public static final float GRAY_BRICK_PROBABILITY = 0.2f;
 	public static final int GRAY_LIVES = 1;
 	public static final int NORMAL_LIVES = 0;
 	
